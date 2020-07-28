19b43226ec1608ec1dba5f0b224656a0e3a6b6b8
amal.chaudhry <amal.chaudhary@experian.com>
2020-07-28 18:40:02 +0530

    add maven test project

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,38 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+    @Test
+    public void sum()
+    {
+        System.out.println("SUM");
+        int a = 10 ;
+        int b = 20 ;
+        Assert.assertEquals(30 , b+a);
+    }
+    @Test
+    public void minus()
+    {
+        System.out.println("SUB");
+        int a = 10 ;
+        int b = 20 ;
+        Assert.assertEquals(10 , b-a);
+    }
+    @Test
+    public void multiply()
+    {
+        System.out.println("MUL");
+        int a = 10 ;
+        int b = 20 ;
+        Assert.assertEquals(200 , b*a);
+    }
+    @Test
+    public void divide()
+    {
+        System.out.println("DIV");
+        int a = 10 ;
+        int b = 20 ;
+        Assert.assertEquals(2 , b/a);
+    }
+}
